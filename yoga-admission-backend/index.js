e45b0828c39b0a760192712732c8ba4ba22f13e9
unknown <abhivyas137@gmail.com>
2023-12-19 23:39:32 +0530

    final change

--- yoga-admission-backend/index.js
+++ yoga-admission-backend/index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-// Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/yoga", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,11 +22,12 @@ conn.on('error', (error) => {
   process.exit(1);
 });
 
-// Define User Schema and Model
+
 const UserSchema = new mongoose.Schema({
   name: String,
   age: Number,
-  selectedBatch: String, // Add your batch field
+  email:String,
+  selectedBatch: String,
 });
 
 const UserModel = mongoose.model("users", UserSchema);
@@ -44,27 +44,27 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
-// API endpoint for user registration
+
 app.post('/api/register', async (req, res) => {
   try {
-    const { name, age, selectedBatch } = req.body;
+    const { name, age, email, selectedBatch } = req.body;
 
-    // Validate data on the server-side
+
     if (!name || !age || !selectedBatch) {
       return res.status(400).json({ error: 'All fields are required' });
     }
 
-    // Additional validation logic for age (adjust as needed)
+
     const ageNumber = parseInt(age, 10);
     if (isNaN(ageNumber) || ageNumber < 18 || ageNumber > 65) {
       return res.status(400).json({ error: 'Invalid age. Must be between 18 and 65' });
     }
 
-    // Store user information in the database
-    const user = new UserModel({ name, age, selectedBatch });
+
+    const user = new UserModel({ name, age, email, selectedBatch });
     await user.save();
 
-    // You can perform additional actions here (e.g., payment processing)
+
 
     return res.status(201).json({ success: true, message: 'User registered successfully' });
   } catch (error) {
@@ -74,7 +74,7 @@ app.post('/api/register', async (req, res) => {
 });
 
 
-//get single user using id
+
 app.get('/profile/:id', async (req, res) => {
   const user = await UserModel.findById(req.params.id)
   res.status(200).json({
@@ -82,8 +82,6 @@ app.get('/profile/:id', async (req, res) => {
     user
   })
 });
-
-//get all users
 
 app.get('/profile', async (req, res) => {
   const user = await UserModel.find({});
@@ -97,3 +95,4 @@ app.get('/profile', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+//final change'
